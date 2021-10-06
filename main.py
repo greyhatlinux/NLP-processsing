586181840a09e2ef40a843268b0e687a3db06a78
Vishal Gorai <vishalgorai93@gmail.com>
2021-10-06 12:47:34 +0530

    added sub-window for inputting link

--- main.py
+++ main.py
@@ -19,6 +19,33 @@ class ui(tk.Frame):
     def init_window(self):
         self.master.title("NLP UI")
 
+        scan_btn = tk.Button(root, text="Scan", command = ui.scan_link)
+        scan_btn.pack()
+
+        exit_btn = tk.Button(root, text="Exit", command=self.quit)
+        exit_btn.pack()
+
+    def scan_link():
+        sub_ui = tk.Tk()
+        w = root.winfo_screenwidth()
+        h = root.winfo_screenmmheight()
+        sub_ui.geometry('%dx%d+%d+%d' % (w/3, h/2, w/3, h/4))
+
+        # sub_ui.geometry('300x100')
+
+
+        txt_input = tk.Text(sub_ui, height= 1.5)
+        txt_input.pack()
+
+        submit_btn = tk.Button(sub_ui, text="Submit Link")
+        submit_btn.pack()
+
+        subExit_btn = tk.Button(sub_ui, text="Exit", command=sub_ui.destroy)
+        subExit_btn.pack()
+
+        link = txt_input.get("1.0", 'end-1c')
+        print(link)
+
     def gui_exit(self):
         exit()
     
